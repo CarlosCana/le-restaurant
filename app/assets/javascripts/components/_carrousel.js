b073f6e2bda9da9658e1e3953c39037ffaf34f84
CarlosCana <carloscana@gmail.com>
2018-08-21 19:07:05 +0200

    Carrousel medium

---
+++ app/assets/javascripts/components/_carrousel.js
@@ -0,0 +1,97 @@
+$(document).ready(function($){
+
+
+
+
+var interval;
+  interval = setInterval(function () {
+    moveRight();
+  }, 3000);
+
+  $('._slider').mouseover(function(){
+    clearInterval(interval);
+  });
+  
+  $('._slider').mouseleave(function(){
+    interval = setInterval(function () {
+      moveRight();
+      }, 3000);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	var imageBox = $('.slider ul'),
+		imageWidth = $('.slider ul li').first().children('img').width(),
+		imageQuantity = $('.slider ul').children('li').length,
+		currentImage = 1;
+
+	imageBox.css('width', imageWidth*imageQuantity);
+	console.log(imageWidth);
+
+	$('.nav button').on('click', function(){ 
+		var whichButton = $(this).data('nav');
+
+		if (whichButton === 'next'){
+			if(currentImage === imageQuantity){
+				currentImage = 1;
+				transition(currentImage, imageWidth);
+			}else {
+				currentImage++;
+				transition(currentImage,imageWidth);
+			}
+			
+		}else if (whichButton === 'prev'){
+			if(currentImage === 1){
+				currentImage = imageQuantity;
+				transition(currentImage, imageWidth);
+			}else{
+				currentImage--;
+				transition(currentImage, imageWidth)
+			}
+		}
+	});
+
+	function transition(currentImageInput, ImageWidthInput){
+		var pxValue = -(currentImageInput-1) * ImageWidthInput
+		
+		imageBox.animate({
+			'left' : pxValue
+		
+   		});
+	}
+});
+
+
+
+var interval;
+  interval = setInterval(function () {
+    moveRight();
+  }, 3000);
+
+  $('._slider').mouseover(function(){
+    clearInterval(interval);
+  });
+  
+  $('._slider').mouseleave(function(){
+    interval = setInterval(function () {
+      moveRight();
+      }, 3000);
+  });
+  
